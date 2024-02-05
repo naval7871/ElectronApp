0988d8d5e6a5aa5c03f41d8356a201144b44480e
naval7871 <navalsunar7871@gmail.com>
2024-02-05 09:17:04 +0530

    branch change test

--- src/main/annotatedFun.ts
+++ src/main/annotatedFun.ts
@@ -5,7 +5,7 @@ export function isAnnotatedOrNotFun(textContent){
 
 try {
     
-let annotatorsDetails = JSON.parse(textContent.split(annotatorDetailsSeparator)[1]);
+let annotatorsDetails = JSON.parse(textContent.split(annotatorDetailsSeparator)[1])
 let SpecificityObj = JSON.parse(contentBtwSeparators([...SpecificityArr, textContent]));        
 
 return !!Number(annotatorsDetails["Time taken to complete the task (in mins)"].trim()) && !! annotatorsDetails["Annotator name"].trim()  && !!SpecificityObj.answer.trim()
